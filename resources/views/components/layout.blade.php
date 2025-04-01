def340437a631496b9b8e9d7b8e0ca8eb683fde8
Micah <ismiki977@gmail.com>
2025-04-01 21:16:53 +0300

    Asset bundling with vite and adding tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 
 <body class="h-full">
